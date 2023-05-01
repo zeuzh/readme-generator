c6a3c4c8cbe85c969c4d930b85ede84b3dbaa134
zeuz <zeuz@zeuzs-iMac.local>
2023-05-01 14:13:02 -0400

    added all questions and set up writeToFile function

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMardown')
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Give a brief description?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How do you install this project?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What are the steps to get this application running?"
+    },
+    {
+        type: "input",
+        name: "credits",
+        message: "Who contributed to this project?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license does this application need",
+        choices: ["MIT", "Apache", "None"]
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "How did you test this application"
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+        if (err) console.log(err)
+        console.log("README generated")
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        writeToFile("README.md", generateMarkdown(data))
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMardown.js
+++ utils/generateMardown.js
@@ -16,6 +16,9 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  # Description
+  ${data.description}
 `;
 }
 
