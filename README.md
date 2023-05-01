4325813805718f1e7276bb02b8c621631e50a399
zeuz <zeuz@zeuzs-iMac.local>
2023-05-01 14:20:35 -0400

    set up string tempalate in generateMarkdown

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# readme-gen
+# test123
+## Description
+asdasdsad
+## Installaion
+asdasdsad
+## Usage
+asdsdaasd
+## Credits
+asdasdasdsad
+## License
+MIT
+## Tests
+sdfsdfsdf
--- utils/generateMardown.js
+++ utils/generateMardown.js
@@ -1,24 +1,35 @@
-
- 
-
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(data) {
+  if (data.license == "Apache") {
+    return `## License
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+`;
+  }
+  if (data.license == "MIT") {
+    return `## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+`;
+  }
+  if (data.license == "None") {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-  # Description
-  ${data.description}
+## Description
+${data.description}
+## Installaion
+${data.installation}
+## Usage
+${data.usage}
+## Credits
+${data.credits}
+${renderLicenseLink(data)}
+## Tests
+${data.tests}
 `;
 }
 
